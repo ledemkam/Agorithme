212a7511d1fcd61568cea9159ddd3c7455509548
ledemkam <ledemkam@gmail.com>
2022-11-13 15:22:08 +0100

    change

--- assets/js/app.js
+++ assets/js/app.js
@@ -18,3 +18,58 @@ function median (arr) {
   }
 
 }
+
+// how to remove space in javascript
+
+function removeSpace(str) {
+  return str.replace(/\s/g, '');
+}
+
+console.log(removeSpace("javascript"));
+console.log(removeSpace("´a b c d e"));
+console.log(removeSpace(" ein katze schläft, die suris tanzen."))
+
+
+//how to add positive numbers to javascript
+
+function addPositive(num) {
+  let total = 0;
+  for (let i = 0; i < num.length; i++) {
+      if(num[i] > 0) {
+          total += num[i]
+      }
+  }
+  return total;
+}
+
+console.log(addPositive([1,3,4,5]));
+console.log(addPositive([-1,-2,-3]));
+console.log(addPositive([0]));
+console.log(addPositive([]));
+
+// or for on ...for tu use ...to add positive number
+
+function addPositive2(sum) {
+  let total = 0;
+  for(n of sum){
+      if(n > 0) {
+          total += n
+      }
+  }
+}
+console.log(addPositive2([1,3,4,5]));
+console.log(addPositive2([-1,-2,-3]));
+console.log(addPositive2([0]));
+console.log(addPositive2([]));
+
+// we cann use method reduce to add positive number
+function addPositive3(arr){
+  return arr.reduce(((acc,current) => current > 0 ? acc + current : acc),0)
+}
+// we cann use method filter to add positive number
+
+function addPositive4(numb){
+  return numb.filter(n => n >= 0).reduce(((a,c) => a + c),0)
+
+}
+
